82b6e5365602c5cbb1dfc73bfc9c0968d8525082
/Fox --develop <paul.liverman.iii@gmail.com>
2017-10-13 22:12:30 -0700

    made header in slam.lua clear about the modifications made

--- slam.lua
+++ slam.lua
@@ -23,6 +23,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 --
+-- (Modified by Guard13007 to remove global state modification.)
 
 local newInstance = love.audio.newSource
 local stop = love.audio.stop
